5378a7960964f5c01460f01e68603ec63ee23c65
LouisOlouis <Lcoliveiraaguiar@gmail.com>
2026-06-24 13:55:09 -0300

    feat(separator): cria o sistema de separaçao de caracteres

--- calc/main.c
+++ calc/main.c
@@ -2,6 +2,41 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+
+char eh_operador(char c) {
+    switch (c) {
+        case '+': case '-': case '*': case '/': 
+        case '%': case '=': case '<': case '>': 
+        case '&': case '|':
+            return true;
+        default:
+            return false;
+    }
+}
+
+typedef struct Separator_returns {
+    char buffer;
+    int position;
+} separator_returns;
+
+separator_returns separator(char s[100], int pos){
+    separator_returns res;
+
+    #define p res.position
+
+    for (p = pos; s[p] != '\0'; p++) {
+        if (isdigit(s[p])) {
+            strcat(res.buffer, s[p]);
+        } else if (eh_operador(s[p])) {
+            return res;
+        } 
+        // else if (s[*p] == '!'){
+        //     return;
+        // }
+    }
+}
+
+
 int main(int argc, char const *argv[])
 {
     if(argc > 1){
@@ -11,30 +46,9 @@ int main(int argc, char const *argv[])
     char s[100];
 
     fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
 
-    char b1, b2;
-
-    char operator;
-
-    int result;
-
-    bool ps = false;
-
-    for (int i = 0; s[i] != '\0'; i++) {
-
-        if (isdigit(s[i])) {
-            if(!ps){strcat(b1, s[i]);}
-            else{strcat(b2,s[i]);};
-        } else if (s[i] == '+' ||
-                   s[i] == '-' ||
-                   s[i] == '*' ||
-                   s[i] == '/') {
-            operator = s[i];
-            ps = true;
-        }
-    }
-
-
+    separator_returns res = separator(s, 0);
 
     return 0;
 }
