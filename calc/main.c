f1c06f3c79a8895d8e6ece42d04ab1e86e04cf09
LouisOlouis <Lcoliveiraaguiar@gmail.com>
2026-07-01 12:09:56 -0300

    -calc fix(eh_operador): removidos casos inuteis

--- calc/main.c
+++ calc/main.c
@@ -13,7 +13,6 @@ char eh_operador(char c) {
     switch (c) {
         case '+': case '-': case '*': case '/': 
         case '%': case '=': case '<': case '>': 
-        case '&': case '|': case '(': case ')':
             return true;
         default:
             return false;
